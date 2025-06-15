6cbf1066d58cdf7b79b73e1f66dcf16851cb0a66
ZecaMonky <zacamonky@gmail.com>
2025-06-15 20:29:42 +0500

    6

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const sequelize = require('./db')
 const errorHandler = require("./middleware/ErrorHandingMiddleware")
 const router = require('./routes/index')
@@ -8,10 +9,20 @@ const router = require('./routes/index')
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+// Статические файлы
+app.use(express.static(path.join(__dirname, '../client/dist')))
+
 app.use('/api', router)
+
+// Обработка всех остальных маршрутов
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/dist', 'index.html'))
+})
+
 app.use(errorHandler)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 const start = async () => {
 	try {
